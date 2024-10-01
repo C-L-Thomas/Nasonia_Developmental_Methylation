2dd0426dbb4608cbdedf8506b02547e2c2632253
Christian Thomas <Christianluthomas@gmail.com>
2024-10-01 15:09:08 +0100

    Update 5.HOMER_Motif_Finder.R

--- Differential_CpG_Analysis/5.HOMER_Motif_Finder.R
+++ Differential_CpG_Analysis/5.HOMER_Motif_Finder.R
@@ -1,1 +1,4 @@
+findMotifsGenome.pl Embryo_Larva_Both.txt \
+~/Nvit_psr_1.1/GCF_009193385.2_Nvit_psr_1.1_genomic.fa \new_Embryo_Larva/ -cpg
 
+#Where Embryo_Larva_Both.txt is a file created with the DMS' with each differentially methylated site being added to the + and - strand as DMS' in this analysis are destranded
